c1c4b67267de7b5723de7e3b63ac51b8cdf104e9
Web Development Dept <webdevelopmentdept@gmail.com>
2026-01-31 17:40:36 +0530

    env access updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   withCredentials: true,
 });
 
