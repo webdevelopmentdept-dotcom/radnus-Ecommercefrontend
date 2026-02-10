da155d406fb35be3fb7f19a6acfde79303a8b3e6
Web Development Dept <webdevelopmentdept@gmail.com>
2026-02-10 15:32:18 +0530

    price details changed

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -174,7 +174,17 @@ useEffect(() => {
   }
 }, [dispatch, product?.category]);
 
-let displayPrice = product?.price ?? 0;
+let displayPrice = 0;
+
+if (userRole === "dealer") {
+  displayPrice = product?.prices?.dealer ?? 0;
+} else if (userRole === "distributor") {
+  displayPrice = product?.prices?.distributor ?? 0;
+} else {
+  // customer / not logged in
+  displayPrice = product?.prices?.customer ?? 0;
+}
+
 
 
 const avgRating =
